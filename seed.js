ac57bc1af38f21f808505ff5598c44079a7d646c
Amy Cheng <109977623+acheng10002@users.noreply.github.com>
2025-10-23 15:28:16 -0400

    Clean up seed script

--- seed.js
+++ seed.js
@@ -1,7 +1,5 @@
-/* *** MINIMAL USER SEED USING PRISMA THEN DISCONNECTS
-imports my initialized Prisma Client instance - object I use to talk to the db 
-- seeds a dev user into db 
-- if using password logins, I need to hash the password on seed */
+/* MINIMAL USER SEED USING PRISMA THEN DISCONNECTS
+imports my initialized Prisma Client instance */
 const prisma = require("./prisma");
 // starts an async Immediately Invoked Function Expression (IIFE)
 (async () => {
@@ -22,16 +20,3 @@ const prisma = require("./prisma");
   }
   // immediately invokes the async function
 })();
-
-/* INSTALLING VIA NODE ECOSYSTEM VS
-- puts JS packages and sometimes thin CLI wrappers into my project or globally
-- CLI wrapper - Node package/CLI that doesn't do the heavy work itself but instead calls
-                an external program or library; it wraps the native tool with a friendly
-                JS API
--- "Node code that drives another program"
-- doesn't give me system binaries unless the package itself provides one (the system
-  binaries are usually Node-based)
-- system binary - compiled executable installed on my OS, accessible to any program via 
-                  my PATH 
--- "that other program" 
-INSTALLING A SYSTEM-LEVEL/NATIVE PROGRAM/LIB VIA MY OS OR VENDOR INSTALLER */
